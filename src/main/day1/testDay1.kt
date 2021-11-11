7ef39f69deeb6ef65ba3684524e344c89341ea2c
JosiahACT <josiah.filleul@weareact.com>
2021-11-11 17:58:01 +0000

    Testing a kotlin file

---
+++ src/main/day1/testDay1.kt
@@ -0,0 +1,16 @@
+package day1
+
+fun main(){
+    findPrimesUpTo100()
+}
+
+fun findPrimesUpTo100(){
+    for ( num in 1.rangeTo(100)){
+        val divisors = mutableListOf<Int>()
+        for (num2 in 1.rangeTo(num)){
+            if (num % num2 == 0) divisors.add(num2)
+        }
+
+        if (divisors.size == 2) println(num)
+    }
+}
